ce4125398fa8ba462a7369251937a6eada45bb93
okushogg <okusho721@gmail.com>
2020-11-16 04:33:37 +0000

    Add create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,36 @@ class BooksController < ApplicationController
   def top
   end
   
-  def new
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create 
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
-  def index
+  def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+  
+  
+  private
+  
+  def book_params
+  params.require(:book).permite(:title,:opinion)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,23 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @books = Book.all
   end
 
   def edit
+    @user = User.find(params[:id])
+    
   end
   
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
-  
+
+
+  private
+  def user_params
+    params.require(:user).permite(:name,:introduction,:profire_image)
+  end
 end
